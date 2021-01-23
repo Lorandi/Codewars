8abda795f880a9a153762f7cc9aa3746cab88e87
Lorandi <69173522+Lorandi@users.noreply.github.com>
2021-01-22 22:23:01 -0300

    Update 29.js

--- 29.js
+++ 29.js
@@ -1,4 +1,5 @@
 //Beginner - Lost Without a Map
+
 const maps = array => array.map(x => x * 2) 
 console.log(maps([1, 2, 3]))            //[2, 4, 6]);
 console.log(maps([4, 1, 1, 1, 4]))      //[8, 2, 2, 2, 8]); 
